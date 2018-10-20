26deaf1f83c8b82a7f80e6a186244ba5d105bbb6
achyutjoshi <achyutjosh1995@gmail.com>
2018-10-20 14:39:52 -0400

    Added fetch_data, requirements.txt

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # Aspect-Sentiment-Analysis-on-Amazon-Reviews
 
-Project Repository for CSE-6242 course. 
+Project Repository for CSE-6242 course.
+
+
+It will be better for us to coordinate different packages using conda environment. Added is the requirements.txt file.
+
+Use this -
+
+conda create --name <env> --file requirements.txt
+
+
+And everytime you commit, run this command in the project folder to update the new set of requirements, if you have installed new libraries for the project -
+
+conda list -e > requirements.txt
+
+
+Ask me(Achyut), if you want to know more about conda environments!
+
+
+## Using fetch_data file
+
+Created a script to download files. Use this in your command line -
+
+fetch_data.py <local>
+
+here <local> is an argument
+1 = Downloads only two files
+0 = Downloads all 46 data files
--- src/data/fetch_data.py
+++ src/data/fetch_data.py
@@ -1,20 +1,63 @@
 # Python script to download reviews data from Amazon S3 bucket
+
+#How to run it?
+# fetch_data.py local
+# local == 1 or 0
+
+
 import requests
 import os
+import csv
+import numpy as np
+import pandas as pd
 import urllib.request
 import gzip
-
-BASE_URL = "https://s3.amazonaws.com/amazon-reviews-pds/tsv/"
-BASE_PATH = os.getcwd()
+import sys
 
 
+def fetch_data(filename ,base_url = "https://s3.amazonaws.com/amazon-reviews-pds/tsv/"):
+    #BASE_URL = "https://s3.amazonaws.com/amazon-reviews-pds/tsv/"
+    BASE_PATH = os.getcwd()
+    response = urllib.request.urlopen(base_url + filename)
+
+    outpath = BASE_PATH + "/data/raw/" + filename[:-3]
+    with open(outpath, 'wb') as outfile:
+        outfile.write(gzip.decompress(response.read()))
+
+    return 1
+
+
+def download_data(local) :
+    BASE_PATH = os.getcwd()
+
+
+    #All the links for the data
+
+    #if local == True, only download selected files
+    if local == 1 :
+        print('Downloading data for local testing')
+
+        links = ["amazon_reviews_us_Electronics_v1_00.tsv.gz","amazon_reviews_us_Kitchen_v1_00.tsv.gz"]
+        for link in links:
+            print(f'Dowloading {link}...')
+            fetch_data(filename=link)
+
+
+    #dowload all 46 files
+    else :
+        print('Beware : Downloading all 46 files!! Terminate if you did not want this.')
+
+        links_file = BASE_PATH + "/data/raw/list_urls.csv"
+        df_links = pd.read_csv(links_file, header = None)
+        assert df_links.shape[0] == 46 , "Check Links file!!"
+        links = df_links[0]
+        for link in links:
+            print(f'Dowloading {link}...')
+            fetch_data(filename=link)
 
 
 
-filename = "amazon_reviews_us_Electronics_v1_00.tsv.gz"
-
-response = urllib.request.urlopen(BASE_URL + filename)
-
-outpath = BASE_PATH + "/data/raw/" + filename[:-3]
-with open(outpath, 'wb') as outfile:
-    outfile.write(gzip.decompress(response.read()))
+if __name__ == '__main__' :
+    #Runner main function for downloading data
+    print(f'You provided {sys.argv[1]} as the argument')
+    download_data(local = int(sys.argv[1]))
