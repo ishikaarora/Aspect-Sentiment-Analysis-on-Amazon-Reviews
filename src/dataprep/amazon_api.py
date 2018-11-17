5fa17f4512d282aa8c1290e4ae4b55192e5b6446
achyutjoshi <achyutjosh1995@gmail.com>
2018-11-17 16:30:27 -0500

    changes made to clean_data

---
+++ src/dataprep/amazon_api.py
@@ -0,0 +1,1 @@
+# Python script for accessing Amazon API
--- src/dataprep/clean_data.py
+++ src/dataprep/clean_data.py
@@ -2,21 +2,23 @@ import pandas as pd
 from sys import argv
 
 #Input file name as argv to run
-file_name = str(argv[1])
 
-def read_data():
-    return pd.read_csv(file_name,
-        sep = "\t", header=0, error_bad_lines = False)
+#Achyut - Commenting this out because repeated
+# def read_data():
+#     return pd.read_csv(file_name,
+#         sep = "\t", header=0, error_bad_lines = False)
 
-if __name__ == '__main__':
-    df = read_data()
+def clean_data(df):
+    print("******Cleaning Started*****")
+
+    print(f'Shape of df before cleaning : {df.shape}')
     df = df[df['review_body'].notna()]
-    df['review_body'] = df['review_body'].str.replace("<br />", "")
-    df['review_body'] = df['review_body'].str.replace("\[?\[.+?\]?\]", "")
+    df['review_body'] = df['review_body'].str.replace("<br />", " ")
+    df['review_body'] = df['review_body'].str.replace("\[?\[.+?\]?\]", " ")
     df['review_body'] = df['review_body'].str.replace("\/{3,}", " ")
-    df['review_body'] = df['review_body'].str.replace("\&\#.+\&\#\d+?;", "")
-    df['review_body'] = df['review_body'].str.replace("\d+\&\#\d+?;", "")
-    df['review_body'] = df['review_body'].str.replace("\&\#\d+?;", "")
+    df['review_body'] = df['review_body'].str.replace("\&\#.+\&\#\d+?;", " ")
+    df['review_body'] = df['review_body'].str.replace("\d+\&\#\d+?;", " ")
+    df['review_body'] = df['review_body'].str.replace("\&\#\d+?;", " ")
 
     #facial expressions
     df['review_body'] = df['review_body'].str.replace("\:\|", "")
@@ -28,10 +30,17 @@ if __name__ == '__main__':
     df['review_body'] = df['review_body'].str.replace("\s{2,}", " ")
 
     df['review_body'] = df['review_body'].str.lower()
-
-    out_name = "CLEANED_"+file_name
-
-    df.to_csv(out_name, sep='\t', index=False)
+    print(f'Shape of df after cleaning : {df.shape}')
+    print("******Cleaning Ended*****")
 
 
-
+    return(df)
+#
+# if __name__ == '__main__':
+#     #file_name = str(argv[1])
+#     #df = read_data()
+#
+#
+#     out_name = "CLEANED_"+file_name
+#
+#     df.to_csv(out_name, sep='\t', index=False)
