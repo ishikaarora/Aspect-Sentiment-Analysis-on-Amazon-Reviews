80e03046061d7e1ebe5ca478ffc163b5c01a8aee
Ishika Arora <ishikaarora4@gmail.com>
2018-11-25 15:10:18 -0500

    Get the cluster name

--- src/models/aspect_clustering.py
+++ src/models/aspect_clustering.py
@@ -79,7 +79,7 @@ def get_cluster_names_map(asp_to_cluster_map, aspect_freq_map):
         this_cluster_asp = [k for k,v in asp_to_cluster_map.items() if v == i]
         filt_freq_map = {k:v for k,v in aspect_freq_map.items() if k in this_cluster_asp}
         filt_freq_map = sorted(filt_freq_map.items(), key = lambda x: x[1], reverse = True)
-        cluster_id_to_name_map[i] = filt_freq_map
+        cluster_id_to_name_map[i] = filt_freq_map[0][0]
 
         # cluster_to_asp_map[i] = this_cluster_asp
 
