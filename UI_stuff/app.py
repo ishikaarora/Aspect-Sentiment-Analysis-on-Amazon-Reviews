42ce627ed34b43d9990c673c9ee937a311ab0fe3
Andrew Giannotto <agiannotto3@gatech.edu>
2018-11-26 22:57:27 -0500

    Connected search bar to review page, cleaned up, need data to proceed

--- UI_stuff/app.py
+++ UI_stuff/app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template, request, json
 app = Flask(__name__)
 
 @app.route('/')
+def search():
+    return render_template('SearchBar.html')
+
+@app.route('/Review')
 def review():
     return render_template('Review.html')
 
